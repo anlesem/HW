ebb3184709fcc567aadf997ff5de906372741d38
anlesem <anlesem@mail.ru>
2022-02-16 17:47:20 +0300

    hw_lesson2

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ export const Header = () => {
 	return (
 		<header className={style.header}>
 			<div className={style.header__wrap + ' container'}>
-				<h1>Урок 1</h1>
+				<h1>Урок 2</h1>
 			</div>
 		</header>
 	);
