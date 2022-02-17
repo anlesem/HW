350ec6511dc204955997901eedae63b04a838cb6
anlesem <anlesem@mail.ru>
2022-02-17 19:56:40 +0300

    1,2,3 через компонент

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,51 +1,44 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 
 import style from './App.module.scss';
 
 import { Header } from '../Header/Header';
 // import { Message } from '../Message/Message';
+import { MessageList } from '../MessageList/MessageList';
+import { Form } from '../Form/Form';
 import { Footer } from '../Footer/Footer';
 
 
 function App() {
-	const [messageList, setMessageList] = useState([]);
-	const robotRef = useRef(null);
+	const [messages, setMessages] = useState([]);
+	// const robotRef = useRef(null);
 
-	const handleClick = (event) => {
-		event.preventDefault();
-		if (event.target.form.elements.author.value || event.target.form.elements.text.value) {
-			setMessageList([...messageList, {
-				author: event.target.form.elements.author.value,
-				text: event.target.form.elements.text.value
+	// text, event для разнообразия
+	const sendMassage = (text, event) => {
+		if (event.target.elements.author.value || event.target.elements.text.value) {
+			setMessages([...messages, {
+				author: text,
+				text: event.target.elements.text.value,
 			}]);
-			event.target.form.elements.author.value = '';
-			event.target.form.elements.text.value = '';
+			event.target.elements.author.value = '';
+			event.target.elements.text.value = '';
 		}
+		// вызов console.log(messages);	не работает
 	}
 
 	useEffect(() => {
-		if (messageList.length > 0) {
-			robotRef.current.insertAdjacentHTML('beforeend', '<p>Спасибо за обращение</p>');
-		}
-	}, [messageList.length]);
+		// вызов console.log(messages);	работает
+	}, [messages]);
+
 
 	return (
 		<div className={style.root}>
 			<Header />
 			<main className="container">
 				{/* <Message content={'Все задания выполнены'} /> */}
-				<form action='#' className={style.form}>
-					<input type='text' name='author' placeholder='ФИО' className={style.form_author}></input>
-					<textarea name='text' className={style.form_text}></textarea>
-					<button className={style.form_button} onClick={(event) => handleClick(event)}>Отправить</button>
-				</form>
-				<ul className={style.messageList}>Результат: {messageList.map((item, idx) => {
-					return <li key={idx} ref={robotRef} className={style.message}>
-						<p className={style.message_text}>{item.text}</p>
-						<p className={style.message_author}>{item.author}</p>
-					</li>;
-				})}
-				</ul>
+
+				<Form addMessage={sendMassage} />
+				<MessageList message={messages} />
 			</main>
 			<Footer />
 		</div>
